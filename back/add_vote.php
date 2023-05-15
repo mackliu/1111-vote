938e6c35111863788bd3bf3a6f86a5bce1dc137a
mackliu <macklun@gmail.com>
2023-05-15 14:58:15 +0800

    簡單的美化一下表單

--- back/add_vote.php
+++ back/add_vote.php
@@ -5,17 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>新增主題</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-<h1>新增主題</h1>
-<form action="../api/add_vote.php" method="post">
-    <div>
-        <label for="subject">主題說明：</label>
-        <input type="text" name="subject" id="subject">
-    </div>
-    <div>
-        <input type="submit" value="新增">
-    </div>
-</form>
+<main>
+
+    <h1>新增主題</h1>
+    <form action="../api/add_vote.php" method="post">
+        <div>
+            <label for="subject">主題說明：</label>
+            <input type="text" name="subject" id="subject" class="subject-input">
+        </div>
+        <div class="time-set">
+            <div class="time-item">
+                <label for="open_time">開放時間：</label>
+                <input type="datetime-local" name="open_time" id="open_time">
+            </div>
+            <div class="time-item">
+                <label for="close_time">關閉時間：</label>
+                <input type="datetime-local" name="close_time" id="close_time">
+            </div>
+        </div>
+        <div>
+            <label for="type">單複選：</label>
+            <input type="radio" name="type" id="type">單選&nbsp;&nbsp;
+            <input type="radio" name="type" id="type">複選
+        </div>
+        <div>
+            <input type="submit" value="新增">
+        </div>
+    </form>
+</main>
 </body>
 </html>
