9a09f5423355c8dc120bc2205bd567fb1b46e9a1
kai <cgk0910@gmail.com>
2023-05-15 13:34:27 +0800

    建立基礎架構所需的頁面及整理專案資料夾結構

--- backend.php
+++ backend.php
@@ -4,17 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>驚奇投票所</title>
+    <title>管理後台</title>
 </head>
 <body>
 <header>
     <a href="index.php">首頁</a>
-    <a href="result.php">結果</a>
-    <a href="login.php">登入</a>
-    <a href="reg.php">註冊</a>
+    <a href="login.php">登出</a>
 </header>
-<main></main>
+<main>
+    <a href='add_vote.php'>新增投票</a>    
+    <a href='edit_vote.php'>編輯投票</a>    
+    <a href='del_vote.php'>刪除投票</a>    
+    <a href='query_vote.php'>結果查詢</a>    
+    <a href=''></a>    
+</main>
 <footer></footer>
-
 </body>
 </html>
