f649c34c29b45008fcfc78fb34a43cfcf642c8ae
mackliu <macklun@gmail.com>
2023-05-15 16:20:48 +0800

    項目資料表新增主題關聯欄位,完成寫入主題及選項功能

--- api/add_vote.php
+++ api/add_vote.php
@@ -1,10 +1,9 @@
 <?php
 include_once "../db.php";
 
-echo "<pre>";
+/* echo "<pre>";
 print_r($_POST);
-echo "</pre>";
-
+echo "</pre>"; */
 
 $sql_chk_subject="select count(*) from `topics` where subject='{$_POST['subject']}'";
 $chk=$pdo->query($sql_chk_subject)->fetchColumn();
@@ -15,14 +14,20 @@ if($chk>0){
     $sql="INSERT INTO `topics`(`subject`, `open_time`, `close_time`, `type`) 
         VALUES ('{$_POST['subject']}','{$_POST['open_time']}','{$_POST['close_time']}','{$_POST['type']}')";
     $pdo->exec($sql);
+
+    //寫入選項
+    $sql_subject_id="select `id` from `topics` where `subject`='{$_POST['subject']}'";
+    //echo $sql_subject_id;
+    $subject_id=$pdo->query($sql_subject_id)->fetchColumn();
+    
+    //echo $subject_id;
+
+    foreach($_POST['description'] as $desc){
+        if($desc!=''){
+            $sql_option="INSERT INTO `options`(`description`,`subject_id`) 
+                       VALUES ('$desc','$subject_id')";
+            $pdo->exec($sql_option);
+        }
+    }
 }
-
-//寫入選項
-$sql_subject_id="select `id` from `topics` where `subject`='{$_POST['subject']}'";
-echo $sql_subject_id;
-$subject_id=$pdo->query($sql_subject_id)->fetchColumn();
-
-echo $subject_id;
-
-
-//header("location:../backend.php");
+header("location:../backend.php");
--- db/vote.sql
+++ db/vote.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2023-05-15 08:04:11
+-- 產生時間： 2023-05-15 10:14:29
 -- 伺服器版本： 10.4.27-MariaDB
 -- PHP 版本： 8.2.0
 
@@ -60,6 +60,7 @@ CREATE TABLE `member` (
 CREATE TABLE `options` (
   `id` int(10) UNSIGNED NOT NULL,
   `description` text NOT NULL,
+  `subject_id` int(10) UNSIGNED NOT NULL,
   `total` int(10) UNSIGNED NOT NULL DEFAULT 0,
   `created_time` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
