61adef930e070bead45fb25a984ee7c14b97e0e4
mackliu <macklun@gmail.com>
2023-05-19 13:45:19 +0800

    先進行資料表的id和表單id的比對,確認那些選項資料是要刪除的

--- api/edit_vote.php
+++ api/edit_vote.php
@@ -11,5 +11,46 @@ $sql="update `topics`
 
 $pdo->exec($sql);
 
+$options=$pdo->query("select `id` from `options` where `subject_id`='{$_POST['subject_id']}'")
+             ->fetchAll(PDO::FETCH_ASSOC);
+
+//將資料表中的選項撈出來和表單中的選項id做比對.
+if(!empty($options)){
+    if(isset($_POST['opt_id'])){
+
+        //以迴圈的方式將資料表中的選項id一個一個拿出來做檢查
+        foreach($options as $opt){
+            
+            //如果資料表中的id沒有在表單中的選項id陣列，則刪除
+            if(!in_array($opt['id'],$_POST['opt_id'])){
+               $pdo->exec("delete from `options` where `id`='{$opt['id']}'");
+            }
+        }
+    }else{
+
+        //如果表單選項id沒有傳來表示要刪除全部的選項資料
+        $pdo->exec("delete from `options` where `subject_id`='{$_POST['subject_id']}'");
+    }
+}
+
+
+if(isset($_POST['opt_id'])){
+    foreach($_POST['opt_id'] as $id){
+
+    }
+
+}
+
+
+echo "<pre>";
+print_r($options);
+echo "<pre>";
+echo "<pre>";
+print_r($_POST['description']);
+echo "<pre>";
+echo "<pre>";
+print_r($_POST['opt_id']);
+echo "<pre>";
+
 
 
