c5698cd95754a25663e40c1096b676f8e689df26
mackliu <macklun@gmail.com>
2023-05-19 10:58:49 +0800

    將主題資料撈出並顯示在編輯主題的表單中

--- back/edit_vote.php
+++ back/edit_vote.php
@@ -1,1 +1,83 @@
-編輯資料
+<?php include_once "../db.php";
+$topic=$pdo->query("select * from `topics` where `id`='{$_GET['id']}'")
+           ->fetch(PDO::FETCH_ASSOC);
+
+$options=$pdo->query("select * from `options` where `subject_id`='{$_GET['id']}'")
+             ->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>編輯主題</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="../js/jquery-3.7.0.min.js"></script>
+</head>
+<body>
+<main>
+
+    <h1>編輯
+        主題</h1>
+    <form action="../api/add_vote.php" method="post">
+        <div>
+            <label for="subject">主題說明：</label>
+            <input type="text" 
+                   name="subject" 
+                   id="subject" 
+                   class="subject-input" 
+                   value="<?=$topic['subject'];?>">
+        </div>
+        <div class="time-set">
+            <div class="time-item">
+                <label for="open_time">開放時間：</label>
+                <input type="datetime-local" name="open_time" id="open_time" value="<?=$topic['open_time'];?>">
+            </div>
+            <div class="time-item">
+                <label for="close_time">關閉時間：</label>
+                <input type="datetime-local" name="close_time" id="close_time" value="<?=$topic['close_time'];?>">
+            </div>
+        </div>
+        <div>
+            <label for="type">單複選：</label>
+            <input type="radio" name="type" value="1">單選&nbsp;&nbsp;
+            <input type="radio" name="type" value="2">複選
+        </div>
+        <hr>
+    <div class="options">        
+        <div>
+            <label for="description">項目：</label>
+            <input type="text" name="description[]"  class="description-input">
+            <span onclick="addOption()">+</span>
+            <span onclick="removeOption(this)">-</span>
+        </div>
+    </div>
+        <div>
+            <input type="submit" value="編輯">
+        </div>
+    </form>
+</main>
+</body>
+</html>
+
+<script>
+function addOption(){
+    let opt=`<div>
+                <label for="description">項目：</label>
+                <input type="text" name="description[]"  class="description-input">
+                <span onclick="addOption()">+</span>
+                <span onclick="removeOption(this)">-</span>
+            </div>`
+    $(".options").append(opt);    
+}
+
+function removeOption(el){
+    let dom=$(el).parent()
+    $(dom).remove();
+}
+
+
+</script>
