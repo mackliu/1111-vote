9588e9d84714ff09be19112213cb01c25a98c756
mackliu <macklun@gmail.com>
2023-05-19 09:48:50 +0800

    完成新增投票主題功能,並可在後台顯示主題列表

--- backend.php
+++ backend.php
@@ -1,3 +1,5 @@
+<?php include_once "db.php";?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>管理後台</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
 <header>
@@ -12,11 +15,44 @@
     <a href="login.php">登出</a>
 </header>
 <main>
+<nav>
     <a href='./back/add_vote.php'>新增投票</a>    
     <a href='./back/edit_vote.php'>編輯投票</a>    
     <a href='./back/del_vote.php'>刪除投票</a>    
-    <a href='./back/query_vote.php'>結果查詢</a>    
-    <a href=''></a>    
+    <a href='./back/query_vote.php'>結果查詢</a>
+</nav>
+<ul class="topic-list">
+    <li class="list-row">
+        <div class="list-item-title">主題</div>
+        <div class="list-item-title">狀態</div>
+        <div class="list-item-title">期間</div>
+        <div class="list-item-title">投票數</div>
+        <div class="list-item-title">操作</div>
+    </li>
+<?php
+    $sql="select * from `topics`";
+    $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    foreach($rows as $row){
+?>
+    <li class="list-row">
+        <div class="list-item"><?=$row['subject'];?></div>
+        <div class="list-item"></div>
+        <div class="list-item">
+          <?=$row['open_time'] . " ~ " . $row['close_time'];?>
+        </div>
+        <div class="list-item"></div>
+        <div class="list-item">
+            <button>編輯</button>
+            <button>刪除</button>
+        </div>
+    </li>
+<?php
+    }
+?>
+</ul>    
+    
+
+
 </main>
 <footer></footer>
 </body>
