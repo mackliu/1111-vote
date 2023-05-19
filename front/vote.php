f3e9d1f55c3a9781ba0c3db63ed1da0b5fe7165a
mackliu <macklun@gmail.com>
2023-05-19 16:05:47 +0800

    完成投票顯示畫面

--- front/vote.php
+++ front/vote.php
@@ -2,8 +2,27 @@
 <?php
 
 $topic=$pdo->query("select * from `topics` where `id`='{$_GET['id']}'")->fetch(PDO::FETCH_ASSOC);
-
-
+$options=$pdo->query("select * from `options` where `subject_id`='{$_GET['id']}'")->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <h2><?=$topic['subject'];?></h2>
+<form action="./api/vote.php" method="post">
+<ul>
+<?php
+foreach($options as $idx => $opt){
+    echo "<li>";
+    echo "<input type='radio' name='desc' value='{$opt['id']}'>";
+    echo "<span>".($idx+1).". </span>";
+    echo "<span>{$opt['description']}</span>";
+    echo "</li>";
+}
+?>
+</ul>
+
+<div>
+    <input type="hidden" name="subject_id" value="<?=$_GET['id'];?>">
+    <input type="submit" value="投票">
+    <input type="button" value="取消">
+</div>
+
+</form>
