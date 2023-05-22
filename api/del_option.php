8a3a9b6d1574060e51998242e70ef2109c2920bb
mackliu <macklun@gmail.com>
2023-05-22 16:05:32 +0800

    修正刪除選項的語法順序問題

--- api/del_option.php
+++ api/del_option.php
@@ -1,6 +1,8 @@
 <?php
 include_once "../db.php";
 
+$subject_id=$pdo->query("select `subject_id` from `options` where `id`='{$_GET['id']}'")->fetchColumn();
+
 $pdo->exec("delete from `options` where `id`='{$_GET['id']}'");
 
-header("location:../backend.php");
+header("location:../backend.php?do=result&id=$subject_id"); 
