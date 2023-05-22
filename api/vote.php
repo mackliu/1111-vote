f8c35411b1d245b1e7a5ecaf8fa9d3ab88d2abde
mackliu <macklun@gmail.com>
2023-05-22 08:52:29 +0800

    增加投票後觀看結果頁面，並加入簡單的css美化

--- api/vote.php
+++ api/vote.php
@@ -3,8 +3,8 @@
 include_once "../db.php";
 
 $opt=$_POST['desc'];
-$subject_id=$_POST['subject_id'];
+$subject_id = $_POST['subject_id'];
 
 $pdo->exec("update `options` set `total`=`total`+1 where `id`='$opt'");
 
-header("location:../index.php");
+header("location:../index.php?do=result&id=$subject_id");
