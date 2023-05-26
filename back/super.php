277245fdc9164f240d22c9dd1fe24ea752fafc19
mackliu <macklun@gmail.com>
2023-05-26 09:13:25 +0800

    建立編輯會員權限頁面，權限以下接選單呈現

--- back/super.php
+++ back/super.php
@@ -1,1 +1,56 @@
-超級管理者
+<h1>會員權限管理</h1>
+<style>
+
+.system.admin{
+    border-collapse: collapse;
+    width:360px;
+    padding:15px;
+    box-shadow: 0 0 10px #ccc;
+}
+.system-admin tr:nth-child(1) td{
+    border:1px solid #ccc;
+    padding:5px 10px;
+    background:#ccc;
+    font-weight: 600;
+}
+.system-admin td{
+    border:1px solid #ccc;
+    padding:5px 10px;
+}
+</style>
+<?php
+$sql="select * from `members`";
+$mems=$pdo->query($sql)->fetchAll();
+?>
+<table class="system-admin">
+    <tr>
+        <td>會員</td>
+        <td>權限</td>
+        <td>操作</td>
+    </tr>
+<?php
+foreach($mems as $mem){
+?>
+<tr>
+    <form action="./api/change_pr.php" method="post">
+        <td><?=$mem['name'];?></td>
+        <td>
+            <select name="pr">
+                <option value="super">超級管理員</option>
+                <option value="admin">管理員</option>
+                <option value="member">會員</option>
+            </select>
+        </td>
+        <td>
+            <input type="hidden" name="id" value="<?=$mem['id'];?>">
+            <button type="submit">編輯</button>
+            <button type="button">刪除</button>
+        </td>
+    </form>
+</tr>
+
+
+<?php
+}
+?>
+</table>
