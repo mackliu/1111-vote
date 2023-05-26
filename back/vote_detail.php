a967d84ccb3db56693212c741158e30bb40e0799
mackliu <macklun@gmail.com>
2023-05-26 11:25:49 +0800

    顯示使用者的投票詳細紀錄

--- back/vote_detail.php
+++ back/vote_detail.php
@@ -3,6 +3,30 @@
 $sub_id=$_GET['sub_id'];
 $log_id=$_GET['log_id'];
 $subject=$pdo->query("select `subject` from `topics` where `id`='{$sub_id}'")->fetchColumn();
+$options=$pdo->query("select * from `options` where `subject_id`='{$sub_id}'")->fetchAll(PDO::FETCH_ASSOC);
+
+$log=$pdo->query("select * from `logs` where `id`='{$log_id}'")->fetch(PDO::FETCH_ASSOC);
+$records=unserialize($log['records']);
+/* echo "<pre>";
+print_r($records);
+echo "</pre>"; */
 ?>
 
 <h2><?=$subject;?></h2>
+<ul>
+    <?php
+    foreach($options as $opt){
+
+    ?>
+    <li>
+        <?php
+            if(in_array($opt['id'],$records)){
+                echo "V";
+            }
+        ?>
+        <?=$opt['description'];?>
+    </li>
+    <?php
+    }
+    ?>
+</ul>
